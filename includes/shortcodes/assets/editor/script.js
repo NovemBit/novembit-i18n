9f8055ec0e2e2066c6d534e959de8ebdb3745e4b
diazoxide <aaron.yor@gmail.com>
2019-10-28 14:25:02 +0400

    Added translation editor js asset and CSS.

---
+++ includes/shortcodes/assets/editor/script.js
@@ -0,0 +1,54 @@
+(function () {
+
+    let switchers = document.getElementsByClassName('novembit-i18n-translation-editor');
+
+    for (let i = 0; i < switchers.length; i++) {
+
+        let switcher = switchers[i];
+
+        let title = switcher.dataset.title;
+        let exit_label = switcher.dataset.exit_label;
+
+        switcher.getElementsByClassName('loading')[0].style.display = 'none';
+
+        let urls = window.novembit.i18n.editor.url_translations;
+
+        let label = document.createElement('div');
+        label.classList.add('label');
+
+        if (window.novembit.i18n.editor.is_editor) {
+            let close_link = document.createElement('a');
+            close_link.innerText = exit_label;
+            close_link.setAttribute('href', window.novembit.i18n.orig_request_uri);
+            label.appendChild(close_link);
+        } else {
+            label.innerText = title;
+        }
+
+        switcher.appendChild(label);
+
+        let list = document.createElement('ul');
+        list.classList.add('list');
+
+
+        for (let lang in urls) {
+
+            if (!urls.hasOwnProperty(lang)) continue;
+
+            let url = urls[lang];
+
+            let item = document.createElement('li');
+
+            let link = document.createElement('a');
+
+            link.setAttribute('href', url);
+            link.innerText = window.novembit.i18n.accept_languages[lang];
+            item.appendChild(link);
+
+            list.appendChild(item);
+        }
+
+        label.appendChild(list);
+    }
+
+})();
