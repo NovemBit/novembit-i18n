ecee39c05a7c020b9d594bcc3d26aa60d7357863
diazoxide <aaron.yor@gmail.com>
2019-11-28 14:40:19 +0400

    Remove JS for editor shortcode.

--- includes/shortcodes/Editor.php
+++ includes/shortcodes/Editor.php
@@ -4,6 +4,8 @@
 namespace NovemBit\wp\plugins\i18n\shortcodes;
 
 
+use NovemBit\i18n\Module;
+
 class Editor
 {
     public static $name = 'novembit-i18n-translation-editor';
@@ -16,30 +18,28 @@ class Editor
         /**
          * @Todo remove this line. This is temporary shortcode name (Prisna legacy)
          * */
-        add_action('wp_enqueue_scripts',[self::class,'renderAssets']);
+        add_action('wp_enqueue_scripts', [self::class, 'renderAssets']);
         add_shortcode('edit-translations-menu', [self::class, 'callback']);
         add_shortcode(self::$name, [self::class, 'callback']);
 
     }
 
-    public static function renderAssets(){
+    public static function renderAssets()
+    {
 
         wp_enqueue_style(
-            self::$name.'-style',
+            self::$name . '-style',
             plugins_url('/includes/shortcodes/assets/editor/style.css', NOVEMBIT_I18N_PLUGIN_FILE),
             [],
-            '1.0.5'
-        );
-
-        wp_enqueue_script(
-            self::$name.'-script',
-            plugins_url('/includes/shortcodes/assets/editor/script.js', NOVEMBIT_I18N_PLUGIN_FILE),
-            [],
-            '1.0.5',
-            true
+            '1.0.6'
         );
 
     }
+
+    /**
+     * @param $atts
+     * @return string
+     */
     public static function callback($atts)
     {
         static::$_id++;
@@ -48,18 +48,49 @@ class Editor
             'id' => self::$name . '-' . self::$_id,
             'class' => self::$name,
             'title' => __('Edit Translations', 'novembit-18n'),
-            'exit_label' => __('Exit Translations', 'novembit-18n'),
-            'loading_label' => __('Edit Translations', 'novembit-18n'),
-
+            'exit_label' => __('Exit Translations', 'novembit-18n')
         ), $atts);
 
         $html = sprintf(
-            "<a translate=\"no\" id=\"%s\" class=\"%s\" data-title=\"%s\" data-exit_label=\"%s\"><span class=\"i18n-label\">{$atts['loading_label']}</span></a>",
+            '<a href="%s" translate="no" id="%s" class="%s"><span class="i18n-label">%s</span></a>',
+            Module::instance()->request->isEditor() ? Module::instance()->request->getOrigRequestUri() : '#',
             $atts['id'],
             $atts['class'],
-            $atts['title'],
-            $atts['exit_label']
+            Module::instance()->request->isEditor() ? $atts['exit_label'] : $atts['title']
         );
+
+        $html .= self::_getURLList();
+
+        return $html;
+    }
+
+    /**
+     * Get url list
+     *
+     * @return string
+     * */
+    private static function _getURLList()
+    {
+        $html = '<ul translate="no" class="i18n-list">';
+
+        $urls = Module::instance()->request->getEditorUrlTranslations();
+
+        $languages = Module::instance()->languages->getAcceptLanguages(true);
+
+        foreach ($urls as $code => $url) {
+
+            $name = $languages[$code]['name'] ?? $code;
+            $flag = $languages[$code]['flag'] ?? null;
+
+            $html .= sprintf(
+                '<li><a href="%1$s"><img alt="%3$s" title="%3$s"" src="%2$s"><span>%3$s</span></a></li>',
+                $url,
+                $flag,
+                $name
+            );
+        }
+
+        $html .= "</ul>";
 
         return $html;
     }
