92a068cd2c562b50e867c892f2a35cfa816f7e54
diazoxide <aaron.yor@gmail.com>
2019-12-03 09:51:19 +0400

    Abstract class of integration mechanism

--- includes/integrations/Integration.php
+++ includes/integrations/Integration.php
@@ -4,19 +4,65 @@
 namespace NovemBit\wp\plugins\i18n\integrations;
 
 
+use NovemBit\i18n\system\exception\Exception;
+
 abstract class Integration
 {
 
-    abstract public static function pluginNames(): array;
+    public static $integrations = [];
 
-    abstract protected function init(): void;
+    public static $plugins = [];
 
+    public static $classes = [];
+
+    public static $functions = [];
+
+    public static $rules = [];
+
+    /**
+     * @throws Exception
+     */
+    final public static function runIntegrations()
+    {
+        foreach (static::$integrations as $integration) {
+            $instance = new $integration();
+            if ($instance instanceof Integration) {
+                $instance->run();
+            } else {
+                throw new Exception(sprintf("Class is not instance of '%s'.", Integration::class));
+            }
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
     final public function run()
     {
 
-        $plugins = static::pluginNames();
+        foreach (static::$rules as $rule) {
+            if(is_callable($rule)){
+                if(!call_user_func($rule)){
+                    return;
+                }
+            } elseif(!$rule){
+                return;
+            }
+        }
 
-        foreach ($plugins as $plugin) {
+        foreach (static::$classes as $class) {
+            if (!class_exists($class)) {
+                return;
+            }
+        }
+
+        foreach (static::$functions as $function) {
+            if (!function_exists($function)) {
+                return;
+            }
+        }
+
+        foreach (static::$plugins as $plugin) {
             if (!function_exists('is_plugin_active')
                 || !is_plugin_active($plugin)
             ) {
@@ -26,5 +72,10 @@ abstract class Integration
 
         $this->init();
 
+        static::runIntegrations();
+
     }
+
+    abstract protected function init(): void;
+
 }
