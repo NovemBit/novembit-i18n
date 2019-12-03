86fbd8ac3b733ca451f59516c06d8de30f2fe21f
diazoxide <aaron.yor@gmail.com>
2019-12-03 09:51:39 +0400

    Algolia integration

---
+++ includes/integrations/Algolia.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace NovemBit\wp\plugins\i18n\integrations;
+
+
+use NovemBit\i18n\Module;
+
+class Algolia extends Integration
+{
+
+    public static $integrations = [
+        Algolia\AlgoliaWoocommerceFork::class
+    ];
+
+    public static $rules = [
+        [self::class, 'isI18nInstanceCreated']
+    ];
+
+    public static $plugins = [
+        'search-by-algolia-instant-relevant-results-fork/algolia.php'
+    ];
+
+    public static function isI18nInstanceCreated()
+    {
+        return Module::instance() !== null;
+    }
+
+    public function init(): void
+    {
+
+    }
+}
