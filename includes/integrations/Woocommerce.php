9ce4801a849b6de36882cce93e73cfd757be51a2
diazoxide <aaron.yor@gmail.com>
2019-12-03 09:50:22 +0400

    Woocommerce integration

---
+++ includes/integrations/Woocommerce.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace NovemBit\wp\plugins\i18n\integrations;
+
+
+class Woocommerce extends Integration
+{
+
+    public static $integrations = [
+    ];
+
+    public static $plugins = [
+        'woocommerce/woocommerce.php'
+    ];
+
+    public function init(): void
+    {
+
+    }
+}
