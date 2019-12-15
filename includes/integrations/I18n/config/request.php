a168b101a8584ec4374fab4d5714c68e7892e24c
diazoxide <aaron.yor@gmail.com>
2019-12-16 00:35:03 +0400

    Option integrations for request

--- includes/integrations/I18n/config/request.php
+++ includes/integrations/I18n/config/request.php
@@ -3,13 +3,18 @@
 use NovemBit\i18n\component\request\interfaces\Request;
 use NovemBit\i18n\Module;
 use NovemBit\wp\plugins\i18n\Bootstrap;
+use NovemBit\wp\plugins\i18n\system\Option;
 
 $config =
     [
-        'runtime_dir'=>Bootstrap::RUNTIME_DIR,
-        'restore_non_translated_urls' => true,
+        'runtime_dir' => Bootstrap::RUNTIME_DIR,
+        'restore_non_translated_urls' => new Option(
+            'request_restore_non_translated_urls',
+            true,
+            ['type' => Option::TYPE_BOOL]
+        ),
         'allow_editor' => current_user_can('administrator'),
-        'default_http_host'=>parse_url(site_url(),PHP_URL_HOST),
+        'default_http_host' => parse_url(site_url(), PHP_URL_HOST),
         'source_type_map' => [
             '/sitemap.xml/is' => 'sitemap_xml',
             '/sitemap-index.xml/is' => 'sitemap_xml',
@@ -24,7 +29,8 @@ $config =
 
                 /** @var Request $request */
 
-                if(preg_match('/(емисия-на-данни|data-feed|данни-подаване|zdroj-dat|projekt|datafeed|data-foder|Daten-Feed|daten-feed|Projekt|τροφοδοσία-δεδομένων|alimentación-de-datos|proyecto|andmevoog|projekti|flux-de-données|projet|feed-podataka|podaci-uvlačenja|feed-di-dati|progetto|データフィード|데이터-피드|gegevensfeed|data-toevoer|project|plik-danych|źródło-danych|feed-de-dados|projeto|flux-de-date|подача-данных|данные-подачи|проект|podajanje-podatkov|data-flöde)/i',$_SERVER['REQUEST_URI'])){
+                if (preg_match('/(емисия-на-данни|data-feed|данни-подаване|zdroj-dat|projekt|datafeed|data-foder|Daten-Feed|daten-feed|Projekt|τροφοδοσία-δεδομένων|alimentación-de-datos|proyecto|andmevoog|projekti|flux-de-données|projet|feed-podataka|podaci-uvlačenja|feed-di-dati|progetto|データフィード|데이터-피드|gegevensfeed|data-toevoer|project|plik-danych|źródło-danych|feed-de-dados|projeto|flux-de-date|подача-данных|данные-подачи|проект|podajanje-podatkov|data-flöde)/i',
+                    $_SERVER['REQUEST_URI'])) {
                     return true;
                 }
 
@@ -62,7 +68,7 @@ $config =
 
         }
     ];
-if(Bootstrap::getCachePool()) {
+if (Bootstrap::getCachePool()) {
     $config['cache_pool'] = Bootstrap::getCachePool();
 }
 return $config;
