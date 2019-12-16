56bf139d48aeb5898241ba437875c45adc9ba685
diazoxide <aaron.yor@gmail.com>
2019-12-16 12:14:18 +0400

    Added checkbox and radio buttons support

--- includes/system/Option.php
+++ includes/system/Option.php
@@ -15,6 +15,11 @@ class Option
     const TYPE_BOOL = 'bool';
     const TYPE_TEXT = 'text';
 
+    const MARKUP_CHECKBOX = 'checkbox';
+    const MARKUP_TEXT = 'text';
+    const MARKUP_NUMBER = 'number';
+    const MARKUP_SELECT = 'select';
+
     const METHOD_SINGLE = 'single';
     const METHOD_MULTIPLE = 'multiple';
 
@@ -91,13 +96,15 @@ class Option
         $type = $this->getParam('type', null);
         $method = $this->getParam('method', self::METHOD_SINGLE);
         $values = $this->getParam('values', []);
+        $markup = $this->getParam('markup', null);
 
         $value = $this->getValue();
         $html = '';
 
         switch ($type) {
             case self::TYPE_BOOL:
-                $html .= sprintf('<input id="%s" type="checkbox" name="%s" %s/>',
+                $html .= sprintf('<input class="%s" id="%s" type="checkbox" name="%s" %s/>',
+                    implode(' ', [$type, $method]),
                     $this->getName(),
                     Bootstrap::getOptionName($this->getName()),
                     $this->getValue() ? 'checked' : ''
@@ -105,32 +112,60 @@ class Option
                 break;
             default:
                 if (!empty($values)) {
-                    $html .= sprintf('<select id="%s" name="%s" %s>',
-                        $this->getName(),
-                        Bootstrap::getOptionName($this->getName()) . ($method == self::METHOD_MULTIPLE ? '[]' : ''),
-                        $method == self::METHOD_MULTIPLE ? 'multiple="multiple"' : ''
-                    );
-                    foreach ($values as $key => $_value) {
-                        $html .= sprintf('<option value="%s" %s>%s</option>',
-                            $key,
-                            ($_value == $value || (is_array($value) && in_array($key, $value))) ? 'selected' : '',
-                            $_value
+                    if ($markup == null || $markup == self::MARKUP_SELECT) {
+                        $html .= sprintf('<select class="%s" id="%s" name="%s" %s>',
+                            implode(' ', [$type, $method]),
+                            $this->getName(),
+                            Bootstrap::getOptionName($this->getName()) . ($method == self::METHOD_MULTIPLE ? '[]' : ''),
+                            $method == self::METHOD_MULTIPLE ? 'multiple="multiple"' : ''
                         );
                     }
-                    $html .= '</select>';
+
+                    foreach ($values as $key => $_value) {
+                        if ($markup == null || $markup == self::MARKUP_SELECT) {
+
+                            $html .= sprintf('<option value="%s" %s>%s</option>',
+                                $key,
+                                (($key == $value) || (is_array($value) && in_array($key, $value))) ? 'selected' : '',
+                                $_value
+                            );
+                        } elseif ($markup == self::MARKUP_CHECKBOX) {
+                            if ($method == self::METHOD_MULTIPLE) {
+                                $html .= sprintf('<div><label><input type="checkbox" name="%s" value="%s" %s>%s</label></div>',
+                                    Bootstrap::getOptionName($this->getName()) . ($method == self::METHOD_MULTIPLE ? '[]' : ''),
+                                    $key,
+                                    (($key == $value) || (is_array($value) && in_array($key, $value))) ? 'checked' : '',
+                                    $_value
+                                );
+                            } else{
+                                $html .= sprintf('<div><label><input type="radio" name="%s" value="%s" %s>%s</label></div>',
+                                    Bootstrap::getOptionName($this->getName()),
+                                    $key,
+                                    (($key == $value) || (is_array($value) && in_array($key, $value))) ? 'checked' : '',
+                                    $_value
+                                );
+                            }
+                        }
+                    }
+
+                    if ($markup == null || $markup = self::MARKUP_SELECT) {
+                        $html .= '</select>';
+                    }
+
                 } elseif ($method == self::METHOD_MULTIPLE) {
                     foreach ($this->getValue() as $key => $_value) {
                         if (!empty($_value)) {
-                            $html .= sprintf('<div><input name="%s" type="text" value="%s"><button onclick="this.parentElement.remove()">X</button></div>',
+                            $html .= sprintf('<div class="group"><input name="%s" type="text" value="%s">%s</div>',
                                 Bootstrap::getOptionName($this->getName()) . '[]',
-                                $_value
+                                $_value,
+                                '<button class="button button-secondary" onclick="this.parentElement.remove()">X</button>'
                             );
                         }
                     }
-                    $html .= sprintf('<div><input name="%s" type="text"></div>',
+                    $html .= sprintf('<div class="group"><input name="%s" type="text"></div>',
                         Bootstrap::getOptionName($this->getName()) . '[]'
                     );
-                    $html .= sprintf('<div><button type="button" onclick="%s">Add new</button></div>',
+                    $html .= sprintf('<div class="group"><button class="button button-primary" type="button" onclick="%s">Add new</button></div>',
                         "var c = this.parentElement.previousSibling.cloneNode(true); c.children[0].value=''; this.parentElement.parentElement.insertBefore(c,this.parentElement);");
                 } elseif ($method != self::METHOD_MULTIPLE) {
                     $html .= sprintf('<input id="%s" type="text" name="%s" value="%s"/>',
