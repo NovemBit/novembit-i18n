ac83e5bacea435e2be246d6f6d3efe66c0270464
diazoxide <aaron.yor@gmail.com>
2019-12-19 01:30:59 +0400

    Translation HTML and fragment config

--- includes/integrations/I18n/config/translation/html.php
+++ includes/integrations/I18n/config/translation/html.php
@@ -1,12 +1,12 @@
 <?php
 
 use NovemBit\i18n\component\translation\type\HTML;
-use NovemBit\i18n\system\parsers\xml\Rule;
 use NovemBit\wp\plugins\i18n\Bootstrap;
+use NovemBit\wp\plugins\i18n\system\Option;
 
 $config = [
     'class' => HTML::class,
-    'runtime_dir'=>Bootstrap::RUNTIME_DIR,
+    'runtime_dir' => Bootstrap::RUNTIME_DIR,
     'title_tag_template' => function (array $params) {
         return sprintf(
             '%s | %s, %s',
@@ -17,10 +17,40 @@ $config = [
                 MB_CASE_TITLE, "UTF-8")
         );
     },
-    'xpath_query_map' => include('html/xpath_query_map.php'),
+    'xpath_query_map' => [
+        'ignore' => new Option('html_xpath_query_map_ignore',
+            include('html/xpath_query_map_ignore.php'),
+            ['parent'=>Bootstrap::SLUG,'type' => Option::TYPE_TEXT, 'method' => Option::METHOD_MULTIPLE])
+        ,
+        'accept' => new Option(
+            'html_xpath_query_map_accept',
+            include('html/xpath_query_map_accept.php'),
+            [
+                'parent'=>Bootstrap::SLUG,
+                'type' => Option::TYPE_OBJECT,
+                'method' => Option::METHOD_MULTIPLE,
+                'template' => [
+                    'type' => ['type' => Option::TYPE_TEXT,
+                        'values' => [
+                            'text' => 'Text',
+                            'url' => 'URL',
+                            'sitemap_xml' => 'Sitemap XML',
+                            'xml' => 'XML',
+                            'html' => 'Html',
+                            'html_fragment' => 'Html Fragment',
+                            'json' => 'JSON',
+                            'jsonld' => "Json LD"
+                        ]
+                    ],
+                ],
+                'label' => 'Test',
+                'description' => 'Test.'
+            ]
+        ),
+    ],
     'save_translations' => false,
 ];
-if(Bootstrap::getCachePool()) {
+if (Bootstrap::getCachePool()) {
     $config['cache_pool'] = Bootstrap::getCachePool();
 }
 return $config;
