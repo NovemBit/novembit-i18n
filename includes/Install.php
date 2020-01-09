80102daa42036874b15531ac014a58a75b337d34
diazoxide <aaron.yor@gmail.com>
2020-01-09 16:35:55 +0400

    Installation migration version compare.

--- includes/Install.php
+++ includes/Install.php
@@ -2,65 +2,84 @@
 
 namespace NovemBit\wp\plugins\i18n;
 
-class Install {
+class Install
+{
 
-	private static $filename = '0_novembit_i18n.php';
+    private static $filename = '0_novembit_i18n.php';
 
-	public static function install() {
+    private static $migration_version = "1.0.1";
 
-	    self::migration();
+    public static function install()
+    {
 
-	    self::install_mu_plugin();
-	}
+        self::migration();
 
-	public static function uninstall(){
-		self::uninstall_mu_plugin();
-	}
+        self::install_mu_plugin();
+    }
 
-	private static function install_mu_plugin() {
+    public static function uninstall()
+    {
+        self::uninstall_mu_plugin();
+    }
 
-		$source = dirname( __FILE__ ) . '/../mu-plugins/i18n.php';
-		$target = WPMU_PLUGIN_DIR . '/' . self::$filename;
+    private static function install_mu_plugin()
+    {
 
-		if(!file_exists(WPMU_PLUGIN_DIR) || !is_dir(WPMU_PLUGIN_DIR)){
-		    mkdir(WPMU_PLUGIN_DIR);
+        $source = dirname(__FILE__) . '/../mu-plugins/i18n.php';
+        $target = WPMU_PLUGIN_DIR . '/' . self::$filename;
+
+        if (!file_exists(WPMU_PLUGIN_DIR) || !is_dir(WPMU_PLUGIN_DIR)) {
+            mkdir(WPMU_PLUGIN_DIR);
         }
 
-		if ( ! copy( $source, $target ) ) {
-			add_action( 'admin_notices', function () {
-				?>
+        if (!copy($source, $target)) {
+            add_action('admin_notices', function () {
+                ?>
                 <div class="notice notice-success is-dismissible">
-                    <p><?php _e( 'Can\'t install mu-plugin file!', 'novembit-i18n' ); ?></p>
+                    <p><?php _e('Can\'t install mu-plugin file!', 'novembit-i18n'); ?></p>
                 </div>
-				<?php
-			} );
-		}
-
-	}
-
-	/**
-	 * @return bool
-	 */
-	private static function uninstall_mu_plugin(){
-		$target = WPMU_PLUGIN_DIR . '/' . self::$filename;
-
-		if(unlink($target)){
-		    return true;
+                <?php
+            });
         }
-		return false;
 
-	}
+    }
 
-	private static function migration(){
+    /**
+     * @return bool
+     */
+    private static function uninstall_mu_plugin()
+    {
+        $target = WPMU_PLUGIN_DIR . '/' . self::$filename;
 
-	    $sql = file_get_contents(__DIR__.'/../vendor/novembit/i18n/migrations/structure.sql');
-
-        global $wpdb;
-        try {
-            $wpdb->query($sql);
-        } catch (\Exception $exception){
-
+        if (unlink($target)) {
+            return true;
         }
+        return false;
+
+    }
+
+    /**
+     *
+     */
+    private static function migration()
+    {
+
+        if (Bootstrap::getOption('migration_version', null) != self::$migration_version) {
+
+            $sql = file_get_contents(__DIR__ . '/../vendor/novembit/i18n/migrations/structure.sql');
+
+            global $wpdb;
+
+            try {
+                $wpdb->query($sql);
+
+                Bootstrap::setOption('migration_version', self::$migration_version);
+
+            } catch (\Exception $exception) {
+
+            }
+        }
+
     }
 
 }
