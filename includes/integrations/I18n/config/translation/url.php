e23afd0bd43d41bbc90844499a6fd01e34fe202a
diazoxide <aaron.yor@gmail.com>
2020-01-24 15:33:59 +0400

    Added Google shopping /var/__/__ fields exclusion rules (Related to https://gitlab.com/brandlight/private-source/plugins/woocommerce-product-feeds-fork/issues/3)

--- includes/integrations/I18n/config/translation/url.php
+++ includes/integrations/I18n/config/translation/url.php
@@ -6,12 +6,12 @@ use NovemBit\wp\plugins\i18n\Bootstrap;
 use NovemBit\wp\plugins\i18n\system\Option;
 
 $config = [
-    'class' => URL::class,
-    'runtime_dir' => Bootstrap::RUNTIME_DIR,
-    'path_separator'=>new Option('translation_url_path_separator', '-',
+    'class'            => URL::class,
+    'runtime_dir'      => Bootstrap::RUNTIME_DIR,
+    'path_separator'   => new Option('translation_url_path_separator', '-',
         [
             'parent' => Bootstrap::SLUG,
-            'type' => Option::TYPE_TEXT
+            'type'   => Option::TYPE_TEXT
         ]
     ),
     'path_translation' => new Option(
@@ -19,37 +19,44 @@ $config = [
         true,
         [
             'parent' => Bootstrap::SLUG,
-            'type' => Option::TYPE_BOOL
+            'type'   => Option::TYPE_BOOL
         ]
     ),
 
-    'path_lowercase' => new Option(
+    'path_lowercase'          => new Option(
         'translation_url_path_lowercase',
         true,
         [
             'parent' => Bootstrap::SLUG,
-            'type' => Option::TYPE_BOOL
+            'type'   => Option::TYPE_BOOL
         ]
     ),
-    'url_validation_rules' => [
+    'url_validation_rules'    => [
         'scheme' => [
             '^(https?)?$'
         ],
-        'host' => [
+        'host'   => [
             sprintf("^$|^%s$|^%s$",
                 preg_quote($_SERVER['HTTP_HOST'] ?? ''),
                 preg_quote(parse_url(site_url(), PHP_URL_HOST))
             ),
         ],
-        'path' => [
+        'path'   => [
             /**
              * @todo query string
              * */
             '^.*(?<!\.js|\.css|\.map|\.png|\.gif|\.webp|\.jpg|\.sass|\.less)$'
         ]
+    ],
+    'path_exclusion_patterns' => [
+        /*
+         * For google shopping
+         * */
+        '/\/var\/.*/is'
     ]
 ];
 if (Bootstrap::getCachePool()) {
     $config['cache_pool'] = Bootstrap::getCachePool();
 }
+
 return $config;
