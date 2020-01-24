d5655b7e82e1e82238ec432b225dbc0c3da388af
diazoxide <aaron.yor@gmail.com>
2020-01-24 18:18:02 +0400

    Added Brandlight theme integration to provide some force options settings Related to google product feeds new url structure https://gitlab.com/brandlight/private-source/plugins/woocommerce-product-feeds-fork/issues/3

---
+++ includes/integrations/Brandlight.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace NovemBit\wp\plugins\i18n\integrations;
+
+
+use NovemBit\wp\plugins\i18n\Bootstrap;
+use NovemBit\wp\plugins\i18n\system\Integration;
+use NovemBit\wp\plugins\i18n\system\Option;
+
+class Brandlight extends Integration
+{
+
+    public static $integrations = [
+    ];
+
+    public static $rules = [
+        [self::class, 'isBrandlightTheme']
+    ];
+
+    public static function isBrandlightTheme()
+    {
+        $theme = wp_get_theme();
+        if ($theme->get_template() == 'brandlight') {
+            return true;
+        }
+        return false;
+    }
+
+    public function init(): void
+    {
+        /*
+         * Google shopping `var` exclusion rules
+         * */
+        add_filter(
+            Option::getOptionFilterName('translation_url_path_exclusion_patterns', Bootstrap::SLUG),
+            function ($patterns) {
+                if (array_search('/\/var\/.*/is', $patterns) === false) {
+                    $patterns[] = '/\/var\/.*/is';
+                }
+                return $patterns;
+            }
+        );
+    }
+
+    protected function adminInit(): void
+    {
+        // TODO: Implement adminInit() method.
+    }
+}
