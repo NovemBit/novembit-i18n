b5b43c0fcf2e83200cbe9be04ef421087ddeec54
diazoxide <aaron.yor@gmail.com>
2020-01-24 18:16:34 +0400

    Setting `path_exclusion_patterns` dynamic option Related to url structure and `var params` translation exclusions https://gitlab.com/brandlight/private-source/plugins/woocommerce-product-feeds-fork/issues/3

--- includes/integrations/I18n/config/translation/url.php
+++ includes/integrations/I18n/config/translation/url.php
@@ -1,4 +1,5 @@
 <?php
+
 defined('ABSPATH') || exit;
 
 use NovemBit\i18n\component\translation\type\URL;
@@ -6,12 +7,13 @@ use NovemBit\wp\plugins\i18n\Bootstrap;
 use NovemBit\wp\plugins\i18n\system\Option;
 
 $config = [
-    'class'            => URL::class,
-    'runtime_dir'      => Bootstrap::RUNTIME_DIR,
-    'path_separator'   => new Option('translation_url_path_separator', '-',
+    'class' => URL::class,
+    'runtime_dir' => Bootstrap::RUNTIME_DIR,
+    'path_separator' => new Option(
+        'translation_url_path_separator', '-',
         [
             'parent' => Bootstrap::SLUG,
-            'type'   => Option::TYPE_TEXT
+            'type' => Option::TYPE_TEXT
         ]
     ),
     'path_translation' => new Option(
@@ -19,41 +21,41 @@ $config = [
         true,
         [
             'parent' => Bootstrap::SLUG,
-            'type'   => Option::TYPE_BOOL
+            'type' => Option::TYPE_BOOL
         ]
     ),
 
-    'path_lowercase'          => new Option(
+    'path_lowercase' => new Option(
         'translation_url_path_lowercase',
         true,
         [
             'parent' => Bootstrap::SLUG,
-            'type'   => Option::TYPE_BOOL
+            'type' => Option::TYPE_BOOL
         ]
     ),
-    'url_validation_rules'    => [
+    'url_validation_rules' => [
         'scheme' => [
             '^(https?)?$'
         ],
-        'host'   => [
-            sprintf("^$|^%s$|^%s$",
+        'host' => [
+            sprintf(
+                "^$|^%s$|^%s$",
                 preg_quote($_SERVER['HTTP_HOST'] ?? ''),
                 preg_quote(parse_url(site_url(), PHP_URL_HOST))
             ),
         ],
-        'path'   => [
+        'path' => [
             /**
              * @todo query string
              * */
             '^.*(?<!\.js|\.css|\.map|\.png|\.gif|\.webp|\.jpg|\.sass|\.less)$'
         ]
     ],
-    'path_exclusion_patterns' => [
-        /*
-         * For google shopping
-         * */
-        '/\/var\/.*/is'
-    ]
+    'path_exclusion_patterns' => new Option(
+        'translation_url_path_exclusion_patterns',
+        [],
+        ['parent' => Bootstrap::SLUG, 'type' => Option::TYPE_TEXT, 'method' => Option::METHOD_MULTIPLE]
+    )
 ];
 if (Bootstrap::getCachePool()) {
     $config['cache_pool'] = Bootstrap::getCachePool();
