82578f27ad9bc6cff2fad895a3fbb5b5d9583883
diazoxide <aaron.yor@gmail.com>
2020-03-09 15:40:51 +0400

    Dynamic countries list

--- includes/integrations/I18n/config/localization.php
+++ includes/integrations/I18n/config/localization.php
@@ -11,13 +11,17 @@ if ( ! class_exists(NovemBit\i18n\system\helpers\Languages::class)) {
     include "vendor/novembit/i18n/src/system/helpers/Languages.php";
 }
 
+if ( ! class_exists(NovemBit\i18n\system\helpers\Countries::class)) {
+    include "vendor/novembit/i18n/src/system/helpers/Countries.php";
+}
+
 $languages_list = Languages::getMap('alpha1', 'name');
 $countries_list = Countries::getMap('alpha2', 'name');
 
 $config =
     [
-        'runtime_dir'      => Bootstrap::RUNTIME_DIR,
-        'from_language'    => new Option(
+        'runtime_dir'         => Bootstrap::RUNTIME_DIR,
+        'from_language'       => new Option(
             'from_language',
             'en',
             [
@@ -29,7 +33,7 @@ $config =
                 'description' => 'Website main content language.'
             ]
         ),
-        'accept_languages' => new Option(
+        'accept_languages'    => new Option(
             'accept_languages',
             [
                 'cs',
@@ -57,36 +61,35 @@ $config =
                 'parent'      => Bootstrap::SLUG,
                 'type'        => Option::TYPE_TEXT,
                 'method'      => Option::METHOD_MULTIPLE,
-                'markup'      => Option::MARKUP_CHECKBOX,
+                'markup'      => Option::MARKUP_SELECT,
+                'main_params' => ['style' => 'grid-template-columns: repeat(1, 1fr);'],
                 'values'      => $languages_list,
                 'label'       => 'To languages',
                 'description' => 'In what languages the site should be translated.'
-
             ]
         ),
-
         'localization_config' => new Option(
             'languages_localization_config',
             [
                 'default'   => ['language' => 'en', 'country' => 'UK', 'region' => 'Europe'],
-                '^.*\.uk$'  => ['language' => 'en', 'country' => 'UK'],
-                '^.*\.ca$'  => ['language' => 'en', 'country' => 'Canada'],
-                '^.*\.ro$'  => ['language' => 'ro', 'country' => 'Romania'],
-                '^.*\.gr$'  => ['language' => 'el', 'country' => 'Greece'],
-                '^.*\.sg$'  => ['language' => 'en', 'country' => 'Singapore'],
-                '^.*\.fr$'  => ['language' => 'fr', 'country' => 'France'],
-                '^.*\.it$'  => ['language' => 'it', 'country' => 'Italy'],
-                '^.*\.nl$'  => ['language' => 'nl', 'country' => 'Netherlands'],
-                '^.*\.de$'  => ['language' => 'de', 'country' => 'Germany'],
-                '^.*\.ru$'  => ['language' => 'ru', 'country' => 'Russia'],
-                '^.*\.dk$'  => ['language' => 'da', 'country' => 'Denmark'],
-                '^.*\.cz$'  => ['language' => 'cs', 'country' => 'Czech Republic'],
-                '^.*\.pl$'  => ['language' => 'pl', 'country' => 'Poland'],
-                '^.*\.nz$'  => ['language' => 'en', 'country' => 'New Zealand'],
-                '^.*\.si$'  => ['language' => 'sl', 'country' => 'Slovenia'],
-                '^.*\.kr$'  => ['language' => 'ko', 'country' => 'South Korea'],
-                '^.*\.ee$'  => ['language' => 'et', 'country' => 'Estonia'],
                 '^.*\.eu$'  => ['language' => 'en', 'region' => 'Europe'],
+                '^.*\.uk$'  => ['language' => 'en', 'country' => 'UK'],
+                '^.*\.ca$'  => ['language' => 'en', 'country' => 'CA'],
+                '^.*\.ro$'  => ['language' => 'ro', 'country' => 'RO'],
+                '^.*\.gr$'  => ['language' => 'el', 'country' => 'GR'],
+                '^.*\.sg$'  => ['language' => 'en', 'country' => 'SG'],
+                '^.*\.fr$'  => ['language' => 'fr', 'country' => 'FR'],
+                '^.*\.it$'  => ['language' => 'it', 'country' => 'IT'],
+                '^.*\.nl$'  => ['language' => 'nl', 'country' => 'NL'],
+                '^.*\.de$'  => ['language' => 'de', 'country' => 'DE'],
+                '^.*\.ru$'  => ['language' => 'ru', 'country' => 'RU'],
+                '^.*\.dk$'  => ['language' => 'da', 'country' => 'DK'],
+                '^.*\.cz$'  => ['language' => 'cs', 'country' => 'CZ'],
+                '^.*\.pl$'  => ['language' => 'pl', 'country' => 'PL'],
+                '^.*\.nz$'  => ['language' => 'en', 'country' => 'NZ'],
+                '^.*\.si$'  => ['language' => 'sl', 'country' => 'SI'],
+                '^.*\.kr$'  => ['language' => 'ko', 'country' => 'KP'],
+                '^.*\.ee$'  => ['language' => 'et', 'country' => 'EE'],
                 '^.*\.com$' => ['language' => 'en', 'country' => 'UK'],
                 '^.*\.net$' => ['language' => 'en', 'country' => 'UK'],
                 '^.*\.org$' => ['language' => 'en', 'country' => 'UK'],
