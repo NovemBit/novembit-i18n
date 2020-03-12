9af37bafd791613f480b7391b4cdb1cc06142297
diazoxide <aaron.yor@gmail.com>
2020-03-12 19:00:22 +0400

    Remove localization config from langs

--- includes/integrations/I18n/config/localization/languages.php
+++ includes/integrations/I18n/config/localization/languages.php
@@ -45,7 +45,6 @@ return [
             'en'
         ],
         [
-
             'type'        => Option::TYPE_TEXT,
             'method'      => Option::METHOD_MULTIPLE,
             'markup'      => Option::MARKUP_SELECT,
@@ -55,62 +54,6 @@ return [
             'description' => 'In what languages the site should be translated.'
         ]
     ),
-    'localization_config' => new Option(
-        'languages_localization_config',
-        [
-            'default'   => ['language' => 'en', 'country' => 'UK', 'region' => 'Europe'],
-            '^.*\.eu$'  => ['language' => 'en', 'region' => 'Europe'],
-            '^.*\.uk$'  => ['language' => 'en', 'country' => 'UK'],
-            '^.*\.ca$'  => ['language' => 'en', 'country' => 'CA'],
-            '^.*\.ro$'  => ['language' => 'ro', 'country' => 'RO'],
-            '^.*\.gr$'  => ['language' => 'el', 'country' => 'GR'],
-            '^.*\.sg$'  => ['language' => 'en', 'country' => 'SG'],
-            '^.*\.fr$'  => ['language' => 'fr', 'country' => 'FR'],
-            '^.*\.it$'  => ['language' => 'it', 'country' => 'IT'],
-            '^.*\.nl$'  => ['language' => 'nl', 'country' => 'NL'],
-            '^.*\.de$'  => ['language' => 'de', 'country' => 'DE'],
-            '^.*\.ru$'  => ['language' => 'ru', 'country' => 'RU'],
-            '^.*\.dk$'  => ['language' => 'da', 'country' => 'DK'],
-            '^.*\.cz$'  => ['language' => 'cs', 'country' => 'CZ'],
-            '^.*\.pl$'  => ['language' => 'pl', 'country' => 'PL'],
-            '^.*\.nz$'  => ['language' => 'en', 'country' => 'NZ'],
-            '^.*\.si$'  => ['language' => 'sl', 'country' => 'SI'],
-            '^.*\.kr$'  => ['language' => 'ko', 'country' => 'KP'],
-            '^.*\.ee$'  => ['language' => 'et', 'country' => 'EE'],
-            '^.*\.com$' => ['language' => 'en', 'country' => 'UK'],
-            '^.*\.net$' => ['language' => 'en', 'country' => 'UK'],
-            '^.*\.org$' => ['language' => 'en', 'country' => 'UK'],
-        ],
-        [
-
-            'main_params' => ['style' => 'grid-template-columns: repeat(3, 1fr);'],
-            'type'        => Option::TYPE_OBJECT,
-            'method'      => Option::METHOD_MULTIPLE,
-            'template'    => [
-                'language'         => [
-                    'type'   => Option::TYPE_TEXT,
-                    'values' => $this->languages->getList(),
-                    'label'  => 'Language'
-                ],
-                'accept_languages' => [
-                    'type'        => Option::TYPE_TEXT,
-                    'method'      => Option::METHOD_MULTIPLE,
-                    'markup'      => Option::MARKUP_SELECT,
-                    'values'      => $this->languages->getList(),
-                    'label'       => 'To languages',
-                    'description' => 'In what languages the site should be translated.'
-                ],
-                'country'          => [
-                    'type'   => Option::TYPE_TEXT,
-                    'label'  => 'Country',
-                    'values' => $this->countries->getList()
-                ],
-                'region'           => ['type' => Option::TYPE_TEXT, 'label' => 'Region'],
-            ],
-            'label'       => 'Language detection pattern',
-        ]
-    ),
-
     'path_exclusion_patterns' => [
         '.*\.php',
         '.*wp-admin',
