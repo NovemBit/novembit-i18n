2193c83808d3e5d62bedf8226ec42ceca94e7fc4
diazoxide <aaron.yor@gmail.com>
2020-03-12 18:59:45 +0400

    Move localization config from lang

--- includes/integrations/I18n/config/localization.php
+++ includes/integrations/I18n/config/localization.php
@@ -4,21 +4,76 @@
 
 defined('ABSPATH') || exit;
 
+use diazoxide\wp\lib\option\Option;
 use NovemBit\wp\plugins\i18n\Bootstrap;
 use NovemBit\wp\plugins\i18n\integrations\I18n;
 
-if (! class_exists(NovemBit\i18n\system\helpers\Languages::class)) {
+if ( ! class_exists(NovemBit\i18n\system\helpers\Languages::class)) {
     include "vendor/novembit/i18n/src/system/helpers/Languages.php";
 }
 
-if (! class_exists(NovemBit\i18n\system\helpers\Countries::class)) {
+if ( ! class_exists(NovemBit\i18n\system\helpers\Countries::class)) {
     include "vendor/novembit/i18n/src/system/helpers/Countries.php";
 }
 
 $config =
     [
         'runtime_dir' => Bootstrap::RUNTIME_DIR,
-        'languages'   => require_once('localization/languages.php')
+
+        'languages' => require_once('localization/languages.php'),
+        'regions'   => require_once('localization/regions.php'),
+        'countries' => require_once('localization/countries.php'),
+
+        'localization_config' => new Option(
+            'languages_localization_config',
+            [
+                'default'   => ['language' => 'en', 'country' => 'UK', 'region' => 'Europe'],
+                '^.*\.eu$'  => ['language' => 'en', 'region' => 'Europe'],
+                '^.*\.uk$'  => ['language' => 'en', 'country' => 'UK'],
+                '^.*\.ca$'  => ['language' => 'en', 'country' => 'CA'],
+                '^.*\.ro$'  => ['language' => 'ro', 'country' => 'RO'],
+                '^.*\.gr$'  => ['language' => 'el', 'country' => 'GR'],
+                '^.*\.sg$'  => ['language' => 'en', 'country' => 'SG'],
+                '^.*\.fr$'  => ['language' => 'fr', 'country' => 'FR'],
+                '^.*\.it$'  => ['language' => 'it', 'country' => 'IT'],
+                '^.*\.nl$'  => ['language' => 'nl', 'country' => 'NL'],
+                '^.*\.de$'  => ['language' => 'de', 'country' => 'DE'],
+                '^.*\.ru$'  => ['language' => 'ru', 'country' => 'RU'],
+                '^.*\.dk$'  => ['language' => 'da', 'country' => 'DK'],
+                '^.*\.cz$'  => ['language' => 'cs', 'country' => 'CZ'],
+                '^.*\.pl$'  => ['language' => 'pl', 'country' => 'PL'],
+                '^.*\.nz$'  => ['language' => 'en', 'country' => 'NZ'],
+                '^.*\.si$'  => ['language' => 'sl', 'country' => 'SI'],
+                '^.*\.kr$'  => ['language' => 'ko', 'country' => 'KP'],
+                '^.*\.ee$'  => ['language' => 'et', 'country' => 'EE'],
+                '^.*\.com$' => ['language' => 'en', 'country' => 'UK'],
+                '^.*\.net$' => ['language' => 'en', 'country' => 'UK'],
+                '^.*\.org$' => ['language' => 'en', 'country' => 'UK'],
+            ],
+            [
+
+                'main_params' => ['style' => 'grid-template-columns: repeat(3, 1fr);'],
+                'type'        => Option::TYPE_OBJECT,
+                'method'      => Option::METHOD_MULTIPLE,
+                'template'    => [
+                    'language'         => [
+                        'type'   => Option::TYPE_TEXT,
+                        'values' => $this->languages->getList(),
+                        'label'  => 'Language'
+                    ],
+                    'accept_languages' => [
+                        'type'        => Option::TYPE_TEXT,
+                        'method'      => Option::METHOD_MULTIPLE,
+                        'markup'      => Option::MARKUP_SELECT,
+                        'values'      => $this->languages->getList(),
+                        'label'       => 'To languages',
+                        'description' => 'In what languages the site should be translated.'
+                    ]
+                ],
+                'label'       => 'Language detection pattern',
+            ]
+        ),
+
     ];
 if (Bootstrap::getCachePool()) {
     $config['cache_pool'] = Bootstrap::getCachePool();
