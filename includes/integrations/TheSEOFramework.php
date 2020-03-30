3b665f5d9b4c76a056b6a4ba5a2e7c4eafdec597
diazoxide <aaron.yor@gmail.com>
2020-03-30 12:22:52 +0400

    Generate sitemap index for only active languages.

--- includes/integrations/TheSEOFramework.php
+++ includes/integrations/TheSEOFramework.php
@@ -28,80 +28,97 @@ class TheSEOFramework extends Integration
             function ($map) {
                 $map['/sitemap.xml/is']       = 'sitemap_xml';
                 $map['/sitemap-index.xml/is'] = 'sitemap_xml';
+
                 return $map;
             }
         );
 
-        add_action('init', function () {
+        add_action(
+            'init',
+            function () {
+                if ($_SERVER['REQUEST_URI'] == "/sitemap-index.xml") {
+                    if ( ! headers_sent()) {
+                        \status_header(200);
+                        header('Content-type: text/xml; charset=utf-8', true);
+                    }
 
-            if ($_SERVER['REQUEST_URI'] == "/sitemap-index.xml") {
-                if (! headers_sent()) {
-                    \status_header(200);
-                    header('Content-type: text/xml; charset=utf-8', true);
+                    $dom           = new \DOMDocument();
+                    $dom->version  = "1.0";
+                    $dom->encoding = "utf-8";
+
+                    $sitemapindex = $dom->createElement('sitemapindex');
+                    $sitemapindex->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+
+                    $sitemap_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/sitemap.xml";
+
+                    $languages = Module::instance()->request->getActiveLanguages();
+
+                    if (empty($languages) || Module::instance()->request->isGlobalDomain()) {
+                        $languages = Module::instance()->request->getAcceptLanguages();
+                    }
+
+
+                    $urls = Module::instance()
+                        ->translation
+                        ->setLanguages($languages)
+                        ->url
+                        ->translate([$sitemap_url])[$sitemap_url];
+
+                    foreach ($urls as $lang => $url) {
+                        $sitemap            = $dom->createElement('sitemap');
+                        $loc                = $dom->createElement('loc');
+                        $loc->nodeValue     = $url;
+                        $lastmod            = $dom->createElement('lastmod');
+                        $lastmod->nodeValue = date('c');
+                        $sitemap->appendChild($loc);
+                        $sitemap->appendChild($lastmod);
+                        $sitemapindex->appendChild($sitemap);
+                    }
+
+                    $dom->appendChild($sitemapindex);
+
+                    echo $dom->saveXML();
+
+                    die;
+                }
+            },
+            11
+        );
+
+        add_action(
+            'init',
+            function () {
+                if ( ! function_exists('\the_seo_framework')) {
+                    return;
                 }
 
-                $dom           = new \DOMDocument();
-                $dom->version  = "1.0";
-                $dom->encoding = "utf-8";
+                if (preg_match('/^\/sitemap.xml/', $_SERVER['REQUEST_URI'])) {
+                    if ( ! headers_sent()) {
+                        \status_header(200);
+                        header('Content-type: text/xml; charset=utf-8', true);
+                    }
+                    echo \the_seo_framework()->get_view('sitemap/xml-sitemap');
+                    echo "\n";
+                    die;
+                }
+            },
+            11
+        );
 
-                $sitemapindex = $dom->createElement('sitemapindex');
-                $sitemapindex->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
-
-                $sitemap_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/sitemap.xml";
-
-                $urls = Module::instance()
-                    ->translation
-                    ->setLanguages(
-                        Module::instance()->localization->languages->getAcceptLanguages()
-                    )
-                    ->url
-                    ->translate([$sitemap_url])[$sitemap_url];
-
-                foreach ($urls as $lang => $url) {
-                    $sitemap            = $dom->createElement('sitemap');
-                    $loc                = $dom->createElement('loc');
-                    $loc->nodeValue     = $url;
-                    $lastmod            = $dom->createElement('lastmod');
-                    $lastmod->nodeValue = date('c');
-                    $sitemap->appendChild($loc);
-                    $sitemap->appendChild($lastmod);
-                    $sitemapindex->appendChild($sitemap);
+        add_filter(
+            'robots_txt',
+            function ($output, $public) {
+                $current_domain = $_SERVER['HTTP_HOST'];
+                $default_domain = parse_url(site_url(), PHP_URL_HOST);
+                if ( ! empty($default_domain)) {
+                    $output = str_replace($default_domain, $current_domain, $output);
                 }
 
-                $dom->appendChild($sitemapindex);
-
-                echo $dom->saveXML();
-
-                die;
-            }
-        }, 11);
-
-        add_action('init', function () {
-
-            if (! function_exists('\the_seo_framework')) {
-                return;
-            }
-
-            if (preg_match('/^\/sitemap.xml/', $_SERVER['REQUEST_URI'])) {
-                if (! headers_sent()) {
-                    \status_header(200);
-                    header('Content-type: text/xml; charset=utf-8', true);
-                }
-                echo \the_seo_framework()->get_view('sitemap/xml-sitemap');
-                echo "\n";
-                die;
-            }
-        }, 11);
-
-        add_filter('robots_txt', function ($output, $public) {
-            $current_domain = $_SERVER['HTTP_HOST'];
-            $default_domain = parse_url(site_url(), PHP_URL_HOST);
-            if (! empty($default_domain)) {
-                $output = str_replace($default_domain, $current_domain, $output);
-            }
-
-            return str_replace('sitemap.xml', 'sitemap-index.xml', $output);
-        }, 30, 2);
+                return str_replace('sitemap.xml', 'sitemap-index.xml', $output);
+            },
+            30,
+            2
+        );
     }
 
     protected function adminInit(): void
