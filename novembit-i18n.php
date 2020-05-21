4e6096391f0963c78c0e5a06b2019b9171daa16b
diazoxide <aaron.yor@gmail.com>
2020-05-21 14:37:30 +0400

    Update plugin version

--- novembit-i18n.php
+++ novembit-i18n.php
@@ -4,7 +4,7 @@
  * Plugin Name: WordPress NovemBit i18n
  * Plugin URI:
  * Description: Dom translation
- * Version: 2.4.0
+ * Version: 2.4.1
  * Author: Novembit
  * Author URI:
  * License: GPLv3
