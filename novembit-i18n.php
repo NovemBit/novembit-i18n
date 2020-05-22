3de4da0312be37aac44d7db478db5810d0d4793b
diazoxide <aaron.yor@gmail.com>
2020-05-23 00:48:56 +0400

    Update deps.

--- novembit-i18n.php
+++ novembit-i18n.php
@@ -4,7 +4,7 @@
  * Plugin Name: WordPress NovemBit i18n
  * Plugin URI:
  * Description: Dom translation
- * Version: 2.4.2
+ * Version: 2.4.3
  * Author: Novembit
  * Author URI:
  * License: GPLv3
