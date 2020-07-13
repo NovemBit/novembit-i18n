11389cb929925f15606a2fe7384072826d469392
diazoxide <aaron.yor@gmail.com>
2020-07-13 16:49:16 +0400

    Disable i18n hub logging

--- novembit-i18n.php
+++ novembit-i18n.php
@@ -4,7 +4,7 @@
  * Plugin Name: WordPress NovemBit i18n
  * Plugin URI:
  * Description: Dom translation
- * Version: 2.4.4
+ * Version: 2.4.5
  * Author: Novembit
  * Author URI:
  * License: GPLv3
